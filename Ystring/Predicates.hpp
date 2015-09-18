cc04d37adb0d4bdadd110977a5ebef2a0fc77dd0
Jan Erik Breimo <jebreimo@gmail.com>
2015-09-18 20:06:58 +0200

    Indentation.

--- Ystring/Predicates.hpp
+++ Ystring/Predicates.hpp
@@ -16,9 +16,7 @@
 
 namespace Ystring
 {
-YSTRING_API bool isPathSeparator(uint32_t chr);
-
-YSTRING_API bool isWindowsPathSeparator(uint32_t chr);
-
-YSTRING_API bool isPosixPathSeparator(uint32_t chr);
+    YSTRING_API bool isPathSeparator(uint32_t chr);
+    YSTRING_API bool isWindowsPathSeparator(uint32_t chr);
+    YSTRING_API bool isPosixPathSeparator(uint32_t chr);
 }
