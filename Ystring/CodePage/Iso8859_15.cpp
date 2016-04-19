21ec6d1f53174faa744d6c913bee9532ddcd8cc8
Jan Erik Breimo <jebreimo@gmail.com>
2016-04-19 08:31:03 +0200

    Made codepage creation more thread-safe.
    But now I realize that keeping factories rather than the actual instances in memory is rather wasteful.

--- Ystring/CodePage/Iso8859_15.cpp
+++ Ystring/CodePage/Iso8859_15.cpp
@@ -11,19 +11,22 @@
 
 namespace Ystring { namespace CodePage
 {
-    typedef std::pair<uint8_t, uint32_t> P;
-    const P Iso8859_15_SpecialChars[] = {
-            P(0xA4, 0x20AC), P(0xA6, 0x0160), P(0xA8, 0x0161),
-            P(0xB4, 0x017D), P(0xB8, 0x017E), P(0xBC, 0x0152),
-            P(0xBD, 0x0153), P(0xBE, 0x0178)
-    };
+    namespace
+    {
+        typedef std::pair<uint8_t, uint32_t> P;
+        const P SpecialChars[] = {
+                P(0xA4, 0x20AC), P(0xA6, 0x0160), P(0xA8, 0x0161),
+                P(0xB4, 0x017D), P(0xB8, 0x017E), P(0xBC, 0x0152),
+                P(0xBD, 0x0153), P(0xBE, 0x0178)
+        };
+
+        CodePageFactory factory(std::begin(SpecialChars),
+                                std::end(SpecialChars),
+                                Encoding::ISO_8859_15);
+    }
 
     CodePage makeIso8859_15()
     {
-        static CodePageFactory factory(
-                std::begin(Iso8859_15_SpecialChars),
-                std::end(Iso8859_15_SpecialChars),
-                Encoding::ISO_8859_15);
         return factory.makeCodePage();
     }
 }}
--- Ystring/CodePage/Iso8859_9.cpp
+++ Ystring/CodePage/Iso8859_9.cpp
@@ -11,19 +11,22 @@
 
 namespace Ystring { namespace CodePage
 {
-    typedef std::pair<uint8_t, uint32_t> P;
-    const P Iso8859_9_SpecialChars[] = {
-            P(0xD0, 0x011E), P(0xDD, 0x0130), P(0xDE, 0x015E),
-            P(0xDF, 0x00DF), P(0xF0, 0x011F), P(0xFD, 0x0131),
-            P(0xFE, 0x015F)
-    };
+    namespace
+    {
+        typedef std::pair<uint8_t, uint32_t> P;
+        const P SpecialChars[] = {
+                P(0xD0, 0x011E), P(0xDD, 0x0130), P(0xDE, 0x015E),
+                P(0xDF, 0x00DF), P(0xF0, 0x011F), P(0xFD, 0x0131),
+                P(0xFE, 0x015F)
+        };
+
+        static CodePageFactory factory(std::begin(SpecialChars),
+                                       std::end(SpecialChars),
+                                       Encoding::ISO_8859_9);
+    }
 
     CodePage makeIso8859_9()
     {
-        static CodePageFactory factory(
-                std::begin(Iso8859_9_SpecialChars),
-                std::end(Iso8859_9_SpecialChars),
-                Encoding::ISO_8859_9);
         return factory.makeCodePage();
     }
 }}
