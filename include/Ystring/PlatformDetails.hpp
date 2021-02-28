757e3d5ac0dc6303a18cd6b572baee6508f89232
Kasper Seweryn <github@wvffle.net>
2021-02-28 13:13:53 +0000

    Update PlatformDetails.hpp

--- include/Ystring/PlatformDetails.hpp
+++ include/Ystring/PlatformDetails.hpp
@@ -23,7 +23,7 @@
                       "wchar_t and char16_t can't be the same type.");
         #define YSTRING_CPP11_CHAR_TYPES_SUPPORTED
     #endif
-#elseif __MINGW64_VERSION_MAJOR
+#elseif MINGW32
     #define YSTRING_WCHAR_IS_2_BYTES
     static_assert(!std::is_same<wchar_t, char16_t>::value,
                   "wchar_t and char16_t can't be the same type.");
